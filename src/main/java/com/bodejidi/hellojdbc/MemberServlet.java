df1ece81cad774800e4cfdee0cade6a08cb425a3
Chai Feng <chaifeng@chaifeng.com>
2014-03-13 12:47:16 +0800

    简单实现 MemberServlet 的 doGet 方法

--- src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
+++ src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
@@ -1,7 +1,14 @@
 package com.bodejidi.hellojdbc;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MemberServlet extends HttpServlet {
 
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        resp.getWriter().println("Member Servlet");
+    }
 }
