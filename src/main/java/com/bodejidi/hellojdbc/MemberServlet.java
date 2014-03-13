2ceced278464fb074af04ab374c691e6c9574e0c
Chai Feng <chaifeng@chaifeng.com>
2014-03-13 13:57:31 +0800

    实现了新增 Member 的功能

--- src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
+++ src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
@@ -5,6 +5,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MemberServlet extends HttpServlet {
 
@@ -13,8 +17,60 @@ public class MemberServlet extends HttpServlet {
     }
 
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        resp.getWriter().println(req.getParameter("first_name")
-                                 + " "
-                                 +req.getParameter("last_name"));
+        String firstName = req.getParameter("first_name");
+        String lastName = req.getParameter("last_name");
+
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+        }
+
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            conn =
+                DriverManager.getConnection("jdbc:mysql://localhost/hellojdbc?"
+                                            + "user=root"
+                                            + "&password=");
+            stmt = conn.createStatement();
+            String sql = "INSERT INTO member(first_name, last_name, date_created, last_updated) "
+                + "VALUES('" + firstName + "', '" + lastName + "', now(), now());";
+            System.out.println("SQL: " + sql);
+            stmt.execute(sql);
+            resp.getWriter().println("Add " + firstName + " " + lastName + " success!");
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            resp.getWriter().println("Error!");
+        } finally {
+            // it is a good idea to release
+            // resources in a finally{} block
+            // in reverse-order of their creation
+            // if they are no-longer needed
+
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                    // ignore
+                }
+                stmt = null;
+            }
+
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException sqlEx) {
+                    // ignore
+                }
+                conn = null;
+            }
+        }
     }
 }
