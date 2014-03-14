0f96cc1943d621a18ba2858dbd6d965c9bdfb393
Chai Feng <chaifeng@chaifeng.com>
2014-03-14 10:53:05 +0800

    以表格的形式输出 member 列表

--- src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
+++ src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
@@ -32,17 +32,19 @@ public class MemberServlet extends HttpServlet {
                                             + "user=root"
                                             + "&password=");
 
-            resp.setContentType("text/plain; charset=UTF-8");
+            resp.setContentType("text/html; charset=UTF-8");
             stmt = conn.createStatement();
             String sql = "SELECT * from member";
             System.out.println("SQL: " + sql);
             rs = stmt.executeQuery(sql);
+            resp.getWriter().println("<html><head><title>Member List</title></head><body><h1>Member List</h1><table><tr><td>ID</td><td>Name</td></tr>\n");
             while(rs.next()) {
                 Long id = rs.getLong("id");
                 String firstName = rs.getString("first_name");
                 String lastName = rs.getString("last_name");
-                resp.getWriter().println(id + " : " + firstName + " " + lastName + "\n");
+                resp.getWriter().println("<tr><td>" + id + "</td><td>" + firstName + " " + lastName + "</td></tr>\n");
             }
+            resp.getWriter().println("</table></body></html>");
         } catch (SQLException ex) {
             // handle any errors
             System.out.println("SQLException: " + ex.getMessage());
