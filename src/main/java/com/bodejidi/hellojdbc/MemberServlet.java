436637d9778f36f7e0d5e15c90c5b9befe1988d8
Chai Feng <chaifeng@chaifeng.com>
2014-03-19 16:07:22 +0800

    把 resp.getWriter() 抽取成变量 out

--- src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
+++ src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
@@ -1,6 +1,7 @@
 package com.bodejidi.hellojdbc;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,7 @@ import java.sql.ResultSet;
 public class MemberServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        PrintWriter out = resp.getWriter();
         try {
             // The newInstance() call is a work around for some
             // broken Java implementations
@@ -39,16 +41,16 @@ public class MemberServlet extends HttpServlet {
             if(null == paramId) {
                 System.out.println("SQL: " + sql);
                 rs = stmt.executeQuery(sql);
-                resp.getWriter().println("<html><head><title>Member List</title></head><body><h1>Member List</h1><table border=\"1\"><tr><td>ID</td><td>Name</td></tr>\n");
+                out.println("<html><head><title>Member List</title></head><body><h1>Member List</h1><table border=\"1\"><tr><td>ID</td><td>Name</td></tr>\n");
                 while(rs.next()) {
                     Long id = rs.getLong("id");
                     String firstName = rs.getString("first_name");
                     String lastName = rs.getString("last_name");
-                    resp.getWriter().println("<tr><td><a href=\"?id=" + id + "\">" + id + "</a></td><td>" + firstName + " " + lastName + "</td></tr>\n");
+                    out.println("<tr><td><a href=\"?id=" + id + "\">" + id + "</a></td><td>" + firstName + " " + lastName + "</td></tr>\n");
                 }
-                resp.getWriter().println("</table>");
-                resp.getWriter().println("<p><a href=\".\">Add member</a></p>");
-                resp.getWriter().println("</body></html>");
+                out.println("</table>");
+                out.println("<p><a href=\".\">Add member</a></p>");
+                out.println("</body></html>");
             } else {
                 sql = sql + " WHERE id=" + paramId;
                 System.out.println("SQL: " + sql);
@@ -59,26 +61,26 @@ public class MemberServlet extends HttpServlet {
                 String firstName = rs.getString("first_name");
                 String lastName = rs.getString("last_name");
 
-                resp.getWriter().println("<html><head><title>Member</title></head><body>"
+                out.println("<html><head><title>Member</title></head><body>"
                                          + "  <h1>Member</h1>"
                                          + "  <form action=\"member\" method=\"POST\">"
                                          + "    <table border=\"1\">\n");
-                resp.getWriter().println("      <tr><td>ID</td><td>" + id + "</td></tr>\n");
-                resp.getWriter().println("      <tr><td>First Name</td><td><input type=\"text\" name=\"first_name\" value=\"" + firstName + "\" /></td></tr>\n");
-                resp.getWriter().println("      <tr><td>Last Name</td><td><input type=\"text\" name=\"last_name\" value=\"" + lastName + "\" /></td></tr>\n");
-                resp.getWriter().println("    </table>");
-                resp.getWriter().println("    <input type=\"hidden\" name=\"id\" value=\"" + id + "\" />");
-                resp.getWriter().println("    <input type=\"submit\" value=\"Update\" />");
-                resp.getWriter().println("  </form>");
-                resp.getWriter().println("  <p><a href=\"member\">Member list</a></p>");
-                resp.getWriter().println("</body></html>");
+                out.println("      <tr><td>ID</td><td>" + id + "</td></tr>\n");
+                out.println("      <tr><td>First Name</td><td><input type=\"text\" name=\"first_name\" value=\"" + firstName + "\" /></td></tr>\n");
+                out.println("      <tr><td>Last Name</td><td><input type=\"text\" name=\"last_name\" value=\"" + lastName + "\" /></td></tr>\n");
+                out.println("    </table>");
+                out.println("    <input type=\"hidden\" name=\"id\" value=\"" + id + "\" />");
+                out.println("    <input type=\"submit\" value=\"Update\" />");
+                out.println("  </form>");
+                out.println("  <p><a href=\"member\">Member list</a></p>");
+                out.println("</body></html>");
             }
         } catch (SQLException ex) {
             // handle any errors
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
-            resp.getWriter().println("Error!");
+            out.println("Error!");
         } finally {
             // it is a good idea to release
             // resources in a finally{} block
@@ -144,21 +146,21 @@ public class MemberServlet extends HttpServlet {
                     + "VALUES('" + firstName + "', '" + lastName + "', now(), now());";
                 System.out.println("SQL: " + sql);
                 stmt.execute(sql);
-                resp.getWriter().println("Add " + firstName + " " + lastName + " success!");
-                resp.getWriter().println("<br/><a href=\"\">Member List</a>");
+                out.println("Add " + firstName + " " + lastName + " success!");
+                out.println("<br/><a href=\"\">Member List</a>");
             } else {
                 String sql = "update member set first_name='" + firstName + "', last_name='" + lastName + "' where id="+id;
                 System.out.println("SQL: " + sql);
                 stmt.execute(sql);
-                resp.getWriter().println("Update id=" + id + ": " + firstName + " " + lastName + " success!");
-                resp.getWriter().println("<br/><a href=\"\">Member List</a>");
+                out.println("Update id=" + id + ": " + firstName + " " + lastName + " success!");
+                out.println("<br/><a href=\"\">Member List</a>");
             }
         } catch (SQLException ex) {
             // handle any errors
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
-            resp.getWriter().println("Error!");
+            out.println("Error!");
         } finally {
             // it is a good idea to release
             // resources in a finally{} block
