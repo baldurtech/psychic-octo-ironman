efabad552605213fb17970ff0ad7a9cab371d826
Chai Feng <chaifeng@chaifeng.com>
2014-03-26 16:28:41 +0800

    把 login,showLoginSuccess,showLoginFail三个方法从memberServlet复制过来

--- src/main/java/com/bodejidi/hellojdbc/AuthServlet.java
+++ src/main/java/com/bodejidi/hellojdbc/AuthServlet.java
@@ -18,11 +18,68 @@ public class AuthServlet extends HttpServlet {
         }
     }
 
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        login(req, resp);
+    }
+
     public boolean isNotLogin(HttpServletRequest req)
         throws IOException, ServletException {
 
         Long memberId = (Long)req.getSession().getAttribute("memberId");
         return null == memberId;
+    }
+
+    public void login(HttpServletRequest req, HttpServletResponse resp)
+        throws IOException, ServletException {
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if(username.equalsIgnoreCase("admin") &&
+           password.equals("s3cr3t")) {
+            HttpSession session = req.getSession();
+            session.setAttribute("memberId", 0L);
+
+            showLoginSuccess(req, resp);
+        } else {
+            showLoginFail(req, resp);
+        }
+    }
+
+    public void showLoginFail(HttpServletRequest req, HttpServletResponse resp)
+        throws IOException, ServletException {
+
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html>");
+        out.println("  <head>");
+        out.println("    <title>Login Fail!</title>");
+        out.println("  </head>");
+        out.println("  <body>");
+        out.println("    <h1>Login Fail!</h1>");
+        out.println("  </body>");
+        out.println("</html>");
+
+    }
+
+    public void showLoginSuccess(HttpServletRequest req, HttpServletResponse resp)
+        throws IOException, ServletException {
+
+        Integer timeout = 5;
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("  <head>");
+        out.println("    <meta http-equiv=\"refresh\""
+                    + " content=\"" + timeout + "; URL=member?action=List\">");
+        out.println("    <title>Login Success</title>");
+        out.println("  </head>");
+        out.println("  <body>");
+        out.println("    <h1>Welcome Admin!</h1>");
+        out.println("    Please wait for " + timeout
+                    + " seconds, if not redirect please click ");
+        out.println("    <a href=\"member\">Member list</a>");
+        out.println("  </body>");
+        out.println("</html>");
     }
 
     public void showLoginPage(HttpServletRequest req, HttpServletResponse resp)
@@ -36,7 +93,7 @@ public class AuthServlet extends HttpServlet {
         out.println("  </head>");
         out.println("  <body>");
         out.println("    <h1>Please Login!</h1>");
-        out.println("    <form action=\"member\" method=\"POST\">");
+        out.println("    <form action=\"" + req.getRequestURI() + "\" method=\"POST\">");
         out.println("      <label>Username: <input type=\"text\" name=\"username\"/></label>");
         out.println("      <label>Password: <input type=\"password\" name=\"password\"/></label>");
         out.println("      <input type=\"submit\" name=\"action\" value=\"Login\" />");
