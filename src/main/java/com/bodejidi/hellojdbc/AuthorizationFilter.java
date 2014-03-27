81bd30290475888cce0085bc65e633da455ea1af
Chai Feng <chaifeng@chaifeng.com>
2014-03-27 10:44:58 +0800

    对于非 /auth/ 开头的 URL 要求登录

--- src/main/java/com/bodejidi/hellojdbc/AuthorizationFilter.java
+++ src/main/java/com/bodejidi/hellojdbc/AuthorizationFilter.java
@@ -7,6 +7,9 @@ import javax.servlet.ServletResponse;
 import javax.servlet.FilterConfig;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class AuthorizationFilter implements Filter {
 
@@ -18,11 +21,23 @@ public class AuthorizationFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        if(!req.getRequestURI().startsWith(req.getContextPath() + "/auth/") && isNotLogin(req)) {
+            ((HttpServletResponse)response).sendRedirect(req.getContextPath() + "/auth/login");
+            return;
+        }
         chain.doFilter(request, response);
     }
 
     @Override
     public void destroy() {
         // pass
+    }
+
+    public boolean isNotLogin(HttpServletRequest req)
+        throws IOException, ServletException {
+
+        Long memberId = (Long)req.getSession().getAttribute("memberId");
+        return null == memberId;
     }
 }
