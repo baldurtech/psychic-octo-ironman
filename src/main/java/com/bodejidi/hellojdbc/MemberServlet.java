a7d3355484431a6c2ae0432b8694cebeb766dabf
Chai Feng <chaifeng@chaifeng.com>
2014-03-31 14:52:02 +0800

    把按 id 删除 member 的代码抽取到 deleteById 方法中

--- src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
+++ src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
@@ -150,16 +150,19 @@ public class MemberServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
 
         String id = req.getParameter(MEMBER_FORM_ID);
+        deleteById(Long.valueOf(id));
 
+        req.setAttribute("flash.message", "Delete ID=" + id + " success!");
+        forward("result", req, resp);
+    }
+
+    public void deleteById(Long id) throws SQLException {
         String sql = "DELETE FROM " + MEMBER_TABLE + " where " + MEMBER_ID + "=" + id;
         debug("SQL: " + sql);
 
         DatabaseService ds = DatabaseService.newInstance();
         ds.execute(sql);
         ds.close();
-
-        req.setAttribute("flash.message", "Delete ID=" + id + " success!");
-        forward("result", req, resp);
     }
 
     public void update(HttpServletRequest req, HttpServletResponse resp)
