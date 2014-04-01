f318c7c2b4c22bd1f369c91560021314fc5c8150
Chai Feng <chaifeng@chaifeng.com>
2014-04-01 10:48:13 +0800

    让 ValidateException 变成运行时的异常

--- src/main/java/com/bodejidi/hellojdbc/ValidateException.java
+++ src/main/java/com/bodejidi/hellojdbc/ValidateException.java
@@ -1,6 +1,6 @@
 package com.bodejidi.hellojdbc;
 
-public class ValidateException extends ApplicationException {
+public class ValidateException extends ApplicationRuntimeException {
     public ValidateException(String message) {
         super(message);
     }
